e1f6d635bea4f01c9d3e76e1db087d15b6cd86ec
shivamsk1546 <shivamsk1546@gmail.com>
2024-10-30 17:43:34 +0530

    30/10/2024

---
+++ BACKTRACKING/bOnArray.java
@@ -0,0 +1,29 @@
+package BACKTRACKING;
+
+public class bOnArray {
+
+    public static void changeArr(int arr[],int i, int val){
+        //base case
+        if(i == arr.length){
+            printArr(arr);
+            return;
+        }
+        //reursion (kaam)
+        arr[i] = val;
+        changeArr(arr, i+1, val+1);
+        arr[i] = arr[i] - 2;
+    }
+//TC = O(n);
+//SC = O(n);
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        int arr[] = new int[5];
+        changeArr(arr,0,1);
+        printArr(arr);
+    }
+}
