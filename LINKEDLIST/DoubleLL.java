485e64a3f969c63e8c20d9e6154b4a28f07dfaa4
shivamsk1546 <shivamsk1546@gmail.com>
2025-01-20 17:54:31 +0530

    20jan2025Day0

---
+++ LINKEDLIST/DoubleLL.java
@@ -0,0 +1,27 @@
+package LINKEDLIST;
+
+public class DoubleLL {
+    public class Node{
+        int data;
+        Node next;
+        Node prev;
+
+        public Node(int data){
+            this.data = data;
+            this.next = null;
+            this.prev = null;
+        }
+    }
+
+    public static Node head;
+    public static Node tail;
+    public static int size;
+
+    //add
+
+    //remove
+
+    public static void main(String args[]){
+        DoubleLL dll = new doubleLL();
+    }
+}
